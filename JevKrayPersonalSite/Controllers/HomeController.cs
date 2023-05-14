354595ade0ee62a144e172c51f33386f730a0ad4
Евгений Красовский <semmitwinsboss@gmail.com>
2023-05-14 23:34:29 +0300

    Save 1st page almost done

--- JevKrayPersonalSite/Controllers/HomeController.cs
+++ JevKrayPersonalSite/Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace JevKrayPersonalSite.Controllers
             return View();
         }
 
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
