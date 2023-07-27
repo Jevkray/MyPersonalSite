ed96f22ac0aa8d022077c9306d165a2b34142383
Евгений Красовский <semmitwinsboss@gmail.com>
2023-07-27 16:06:23 +0300

    Add new Project to my Projects page(Steam Checker)

--- JevKrayPersonalSite/Controllers/ProjectsController.cs
+++ JevKrayPersonalSite/Controllers/ProjectsController.cs
@@ -33,6 +33,11 @@ namespace JevKrayPersonalSite.Controllers
             return View();
         }
 
+        public IActionResult SteamChecker()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
