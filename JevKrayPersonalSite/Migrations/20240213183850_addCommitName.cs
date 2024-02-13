629430812a929d3646ecc7089307098a859d8c12
Евгений Красовский <semmitwinsboss@gmail.com>
2024-02-13 22:27:18 +0300

    Fixed UpdatesModel, commit model - and refactoring UpdatesController to remove html code from it. Also some fixes on css, and Routing.

---
+++ JevKrayPersonalSite/Migrations/20240213183850_addCommitName.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JevKrayPersonalSite.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCommitName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Commits",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Commits",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Date", "Name" },
+                values: new object[] { new DateTimeOffset(new DateTime(2024, 2, 13, 21, 38, 49, 937, DateTimeKind.Unspecified).AddTicks(6752), new TimeSpan(0, 3, 0, 0, 0)), "test" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Commits");
+
+            migrationBuilder.UpdateData(
+                table: "Commits",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTimeOffset(new DateTime(2023, 10, 19, 21, 42, 46, 489, DateTimeKind.Unspecified).AddTicks(2126), new TimeSpan(0, 3, 0, 0, 0)));
+        }
+    }
+}
